cfaea973bc5349808c2ccb8cd3b69280572d9e83
sak h <sakh@saks-MacBook-Pro.local>
2025-09-13 16:18:02 +0900

    range式の変換とREADME

--- Spirals/src/WaveHelix/waveHelix.h
+++ Spirals/src/WaveHelix/waveHelix.h
@@ -19,18 +19,18 @@ public:
     float theta = (ofGetElapsedTimef() - 1.0f) * 6.227f;
 
     float startY =  place.y - cylinder.getHeight() / 2;
+
+    float frequency = 0.5f;
     /*
-     * sin(theta)は[-1, 1]になる+1して[0, 2]になる/2して[0, 1]にする。
-     * 関数ないでsin(0) = 0.5になるため位相を sin(0) = -1 + 1にするために-PI/2する。
-     * theta / 2はデザイン一周の同期とかさなるので遅くする。
+     * スタート地点cos(0) = 0にしたい。1 - cos(PI)でPIで1-(-1) = 2 / 2 = 1
+     * をつくる。
      */
-    float yRanage= (sin(theta - PI/2) + 1) / 2;
+    float yRanage= (1 - cos( theta * frequency)) / 2;
     pos.y = ofLerp(startY, startY + cylinder.getHeight(), yRanage);
 
     float polyRadius = radius + Config::Gap;
     pos.x = polyRadius * cos(theta) + place.x;
     pos.z = polyRadius * sin(theta) + place.z;
-     // pos.y = cos(progress) * cylinder.getHeight();
 
     polyLine.addVertex(pos.x, pos.y, pos.z);
 
