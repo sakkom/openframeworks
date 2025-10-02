d6bdc394fcc764d4401d8f03a88fc5fb60a64af7
sak h <sakh@saks-MacBook-Pro.local>
2025-10-02 10:48:50 +0900

    Sketced 縞鋼板

--- Sketch1002/src/ofApp.cpp
+++ Sketch1002/src/ofApp.cpp
@@ -1,75 +1,30 @@
 #include "ofApp.h"
-#include "of3dGraphics.h"
-#include "ofAppRunner.h"
-#include "ofColor.h"
 #include "ofGraphics.h"
 #include "ofMath.h"
-#include "ofShader.h"
 #include "ofUtils.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofSetBackgroundColor(ofColor::black);
-  ofSetLineWidth(5);
-  material.setDiffuseColor(ofFloatColor::white);
-  material.setSpecularColor(ofFloatColor::black);
-  material.setShininess(1);
-  light.setPosition(0,0, 500);
-  // light.enable();
   ofEnableDepthTest();
+  plane.set(500, 500, 100, 100);
+  plane.setMode(OF_PRIMITIVE_TRIANGLES);
+  mesh = plane.getMesh();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+  for(int i = 0; i < mesh.getNumVertices(); i++){
+    glm::vec3 pos = mesh.getVertex(i);
+    float r = sqrt(pos.x * pos.x + pos.y*pos.y);
+    float wave = sin(r * 0.01 + ofGetElapsedTimef()) * 100;
+    mesh.setVertex(i, glm::vec3(pos.x, pos.y, wave));
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
   cam.begin();
-  float spacing = 50.0;
-  float width = ofGetWidth() / 2.0;
-  float height = ofGetHeight() / 2.0;
-
-  int rowIndex = 0;
-  for(float row = -height; row < height; row+= spacing) {
-    for(float col = -width; col < width; col+= spacing * 2) {
-
-      material.begin();
-      ofPushMatrix();
-
-      float rectW = spacing * 1.414;
-      float rectH = spacing / 10;
-
-
-      float r = sqrt(col * col + row * row);
-      float waveSine = sin(r * 0.005 + ofGetElapsedTimef() * 5);
-      float wave = (waveSine > 0) ? 200 : 0;
-
-      if(rowIndex % 2 == 0) {
-        // ofSetColor(255, 200, 200);
-        ofTranslate(col, row, wave);
-        ofRotateZDeg(45);
-        // ofDrawRectangle(-rectW/2, -rectH/2, 0, rectW, rectH);
-        ofDrawEllipse(0, 0, 0, rectW, rectH);
-        // ofDrawLine(-rectW / 2, 0, rectW / 2, 0);
-      } else {
-        // ofSetColor(200, 255, 200);
-        ofTranslate(col + spacing, row, wave);
-        ofRotateZDeg(-45);
-        // ofDrawRectangle(-rectW/2, -rectH/2, 0, rectW, rectH);
-        ofDrawEllipse(0, 0, 0, rectW, rectH);
-        // ofDrawLine(-rectW / 2, 0, rectW / 2, 0);
-      }
-      ofPopMatrix();
-      material.end();
-
-      // ofDrawLine(start, end);
-    }
-    rowIndex++;
-  }
-
-
+  mesh.draw();
   cam.end();
 }
 
