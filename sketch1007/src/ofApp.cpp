438b3beab15a05616159b633c5313ffc0396b614
sak h <sakh@saks-MacBook-Pro.local>
2025-10-07 17:03:29 +0900

    angle seed random値をoffset

--- sketch1007/src/ofApp.cpp
+++ sketch1007/src/ofApp.cpp
@@ -6,7 +6,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+  ofSetBackgroundColor(0);
 }
 
 //--------------------------------------------------------------
@@ -18,25 +18,31 @@ void ofApp::update(){
 void ofApp::draw(){
   ofTranslate(ofGetWidth() / 2.0, ofGetHeight() / 2.0);
   ofPolyline polyline;
-  float radius = 100;
-  for(float j = 0; j < TWO_PI * 3 ; j += TWO_PI / 1000) {
-    int num = int(j / TWO_PI) + 1;
-    float radiusNoise = sin(j * 5) * radius;
-    float mappedRadius = ofMap(radiusNoise, -radius, radius, 5, radius);
-    ofSetRandomSeed(j);
-    // float kaos = abs(sin(ofGetElapsedTimef())) * 100;
-    // float x = cos(j) * mappedRadius + ofRandom(-kaos, kaos);
-    // float y = sin(j) * mappedRadius + ofRandom(-kaos, kaos);
-    // float x = cos(j * num) * mappedRadius ;
-    // float y = sin(j * num) * mappedRadius;
-    float x = cos(j * num) * mappedRadius ;
-    float y = sin(j * num) * mappedRadius;
+  float radius = 150;
+  float radiusX = radius * 1.414;
+  float radiusY = radius * 1;
+  float freq = 3;
+  for(float j = 0; j < TWO_PI * 1 ; j += TWO_PI / 360) {
+    int loop = j / TWO_PI + 1;
+    ofSetRandomSeed(pow(j, 0.8));
+    float radiusNoise = sin(j * freq + loop + ofGetElapsedTimef() * 1) * radius;
+    float mappedRadius = ofMap(radiusNoise, -radius, radius, radius/6, radius);
+    float time = abs(sin(ofGetElapsedTimef() * 2));
+    time = time > 0.66f ? 1 : time > 0.33f ? 0.5 : 0;
+    float kaos = pow(time, 2) * radius ;
+    float x = cos(j) * mappedRadius  * radiusX / radius  + ofRandom(-kaos, kaos);
+    float y = sin(j) * mappedRadius * radiusY / radius  + ofRandom(-kaos, kaos);
     polyline.addVertex( x,  y);
   }
 
+  polyline.close();
+  polyline.draw();
   auto vertices = polyline.getVertices();
   for(auto& vertex : vertices) {
-    ofDrawRectangle(vertex, 1, 5);
+    // ofDrawRectangle(vertex, 1, 5);
+    ofNoFill();
+    ofDrawCircle(vertex, 15);
+    ofFill();
   }
 }
 
