5f06eb5a0f77103cbc0bcdfbed3b44ddde0cfa8b
sak h <sakh@saks-MacBook-Pro.local>
2025-10-08 16:54:56 +0900

    シンプル

--- sketch1007/src/ofApp.cpp
+++ sketch1007/src/ofApp.cpp
@@ -22,19 +22,19 @@ void ofApp::draw(){
   float radius = 100;
   float radiusX = radius * 1.414;
   float radiusY = radius * 1;
-  float petalsLength = ofMap(sin(ofGetElapsedTimef() * 0.5), -1, 1, 1,10);
-  for(float t = 0; t < TWO_PI * 1; t += TWO_PI / (360 * petalsLength)) {
+  float petalsLength = ofMap(sin(ofGetElapsedTimef() * 0.1), -1, 1, 3,3);
+  for(float t = 0; t < TWO_PI * 1; t += TWO_PI / (360 * petalsLength / 2.0)) {
     int loop = t / TWO_PI + 1;
     float petals = sin(t * petalsLength + loop + ofGetElapsedTimef() * 1);
     float staticRadius = ofMap(petals, -1, 1, radius/6, radius);
 
-    // float time = abs(sin(ofGetElapsedTimef() * 2));
-    float time = 0;
+    float time = abs(sin(ofGetElapsedTimef() * 2));
+    // float time = 0;
     time = time > 0.66f ? 1 : time > 0.33f ? 0.5 : 0;
     float kaos = pow(time, 2) * ofGetHeight()/2.0 ;
-    ofSetRandomSeed(pow(t, 0.8)  );
+    ofSetRandomSeed(pow(t, 0.7) + ofGetElapsedTimef());
     float offsetX = ofRandom(-kaos, kaos);
-    ofSetRandomSeed(pow(2*t, 0.8));
+    ofSetRandomSeed(pow(2*t, 0.7) + ofGetElapsedTimef());
     float offsetY = ofRandom(-kaos, kaos);
     float x = cos(t) * staticRadius  * radiusX / radius + offsetX;
     float y = sin(t) * staticRadius * radiusY / radius + offsetY;
@@ -46,8 +46,8 @@ void ofApp::draw(){
   auto vertices = polyline.getVertices();
   for(auto& vertex : vertices) {
     // ofDrawRectangle(vertex, 1, 5);
-    // ofNoFill();
-    ofDrawCircle(vertex, 1);
+    ofNoFill();
+    ofDrawCircle(vertex, 5);
     ofFill();
   }
 }
